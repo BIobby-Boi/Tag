317b054808d32029ed689d32351f33d9c0f9706e
Blobby Boi <131911782+BIobby-Boi@users.noreply.github.com>
2024-09-18 17:32:55 -0700

    Update cloakinnewtab.js

--- cloakinnewtab.js
+++ cloakinnewtab.js
@@ -5,14 +5,14 @@
     var win = window.open();
 
     // Set the icon for the window
-    win.document.head.innerHTML += '<link rel="icon" href="lexialogo.png" type="image/png">';
+    win.document.head.innerHTML += '<link rel="icon" href="https://www.google.com/s2/favicons?sz=64&domain=https://google.com" type="image/png">';
 
     // Set the title of the window
-    win.document.head.innerHTML += '<title>Home - PowerUp</title>';
+    win.document.head.innerHTML += '<title>Google</title>';
 
     // Create an iframe
     var iframe = win.document.createElement('iframe');
-
+    
     // Set styles for the iframe
     iframe.style = "position:fixed;width:100vw;height:100vh;top:0px;left:0px;right:0px;bottom:0px;z-index:2147483647;background-color:white;border:none;";
 
